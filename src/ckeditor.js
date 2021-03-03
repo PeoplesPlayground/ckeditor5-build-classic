8758290ce4da0bed636f15e5a7f802ee18aa00a2
Oleksandr Oleksiv <oleksiv1994@gmail.com>
2021-03-03 16:11:04 +0200

    * new build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -24,7 +24,6 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
 
 import { CustomPlugin } from './customPlugin';
 
@@ -51,8 +50,7 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	CustomPlugin,
 	Table,
-	TableToolbar,
-	FontColor
+	TableToolbar
 ];
 
 // Editor configuration.
